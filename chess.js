94215376a419b5ae83ab272d53694e0514499b21
Jonathan Santos <bitfut@gmail.com>
2022-02-16 18:01:13 +0100

    #1: Cleaning board creation code

--- chess.js
+++ chess.js
@@ -1,35 +1,37 @@
-var t = document.getElementById("tablero");
+var t = document.getElementById("board");
 var ctx = t.getContext("2d");
 
-function cuadrado(color, x, y, width, height)
+const SQUARE_SIZE = 150; // In pixels
+const BOAR_SIZE   = 8;  // Namber of squares in a row and column
+
+function createSquare(color, x, y, width, height)
 {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 }
 
-var columna = 0;
-var lado    = 150;
-var x = 0;
-var y = 0;
-var color1 = "#452a1e";
-var color2 = "#dbb779";
+createBoard();
 
-for(i=0;i<8;i++)
+function createBoard()
 {
-  cuadrado(color1, x, y, lado, lado);
-  x += lado;
+  const lightBrown = "#452a1e";
+  const darkBrown  = "#dbb779";
+  var drawWhite    = false;
 
-  cuadrado(color2, x, y, lado, lado);
-  x += lado;
-
-  if(i == 7 && columna != 8)
+  for(r = 0; r < BOAR_SIZE; r++)
   {
-    var aux = color1;
-    color1  = color2;
-    color2  = aux;
-    x = 0;
-    y += lado;
-    i = 0;
-    columna++;
+    for(c = 0; c < BOAR_SIZE; c++)
+    {
+      if(drawWhite)
+      {
+        createSquare(lightBrown, c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+      }
+      else
+      {
+        createSquare(darkBrown, c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+      }
+      drawWhite = !drawWhite;
+    }
+    drawWhite = !drawWhite;
   }
 }
